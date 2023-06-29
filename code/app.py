880588cc81c03c6b332b3c0a71634ab47ac572d7
GavrilovStepan <gridstepan@yandex.ru>
2023-06-29 22:18:55 +0300

    First commit

---
+++ code/app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return f'Please submit a report'
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
